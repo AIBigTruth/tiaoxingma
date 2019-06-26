7fc5db5590dcd385b23c38e5d32dfd8839c262c9
hhhvvvddd <40601266+hhhvvvddd@users.noreply.github.com>
2019-06-26 15:58:01 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-//主函数
 #include "dialog.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);        //QApplication 类管理图形用户界面应用程序的控制流和主要设置
+    QApplication a(argc, argv);
     Dialog w;
     w.show();
 
